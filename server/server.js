0597fc57bfdb354be000367bddceb73736c295b3
Leah3282 <l0583220680@gmail.com>
2025-01-07 23:56:08 +0200

    make change in branch2

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ app.use("/api/posts", require("./routes/RoutePosts"))
 app.get('/',(req,res)=>{
         res.send("Hello world!!")
 })
-console.log("make change from branch1")
 console.log("make change from branch2")
 
 console.log(process.env.NODE_ENV)
